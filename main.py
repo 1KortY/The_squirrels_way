b7eba813ac6799e2059a287710d842fe77abb697
1KortY <git7krevetka@gmail.com>
2024-01-19 16:26:41 +0300

    Гатова

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ key2 = [1, 2, 3, 4]
 key3 = [1, 2, 3, 4]
 key4 = [1, 2, 3, 4]
 key5 = [1, 2, 3, 4]
-time_3 = 22
+time_3 = 2
 time_4 = 33
 g = []
 k = ['$', '*', '(', ')', '+']
+end = False
 
 
 def load_image(name, colorkey=None):
@@ -38,7 +39,47 @@ def load_image(name, colorkey=None):
 
 def level_completed():
     global g
-    if cur_lvl == 4:
+    if end:
+        restart_button = ImageButton(width / 2 - 300, 480, 252, 74, 'Начать заново', 'data/button.png',
+                                     'data/button_hover.png', 'data/click.mp3')
+        mback_button = ImageButton(width / 2 + 20, 480, 252, 74, 'В меню', 'data/button.png',
+                                   'data/button_hover.png', 'data/click.mp3')
+        running = True
+        while running:
+            screen.blit(load_image('bg_space.jpg'), (0, -12))
+            font = pygame.font.Font(None, 72)
+            text_surface = font.render("Уровень не пройден :(", True, (255, 255, 255))
+            text_rect = text_surface.get_rect(center=(width / 2, 100))
+            screen.blit(text_surface, text_rect)
+            pygame.draw.rect(screen, (0, 1, 51), (width / 2 - 350, 130, 700, 500), 0)
+            pygame.draw.rect(screen, (150, 0, 0), (width / 2 - 350, 130, 700, 500), 8)
+            picture = pygame.image.load('data/game_over.png')
+            picture_rect = picture.get_rect(bottomright=(882, 476))
+            screen.blit(picture, picture_rect)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        menu.fade()
+                        start_screen()
+                if event.type == pygame.USEREVENT and event.button == mback_button:
+                    menu.fade()
+                    return start_screen()
+                if event.type == pygame.USEREVENT and event.button == restart_button:
+                    menu.fade()
+                    return start_game(cur_lvl)
+                for btn in [restart_button, mback_button]:
+                    btn.handle_event(event)
+            for btn in [restart_button, mback_button]:
+                btn.check_hover(pygame.mouse.get_pos())
+                btn.draw(screen)
+            x, y = pygame.mouse.get_pos()
+            screen.blit(cursor, (x - 2, y - 2))
+            pygame.display.flip()
+    elif cur_lvl == 4:
         mback_button = ImageButton(width / 2 - 126, 480, 252, 74, 'В меню', 'data/button.png',
                                    'data/button_hover.png', 'data/click.mp3')
         g.append(cur_lvl)
@@ -458,6 +499,7 @@ def generate_level(level, number):
 
 
 def inscriptions():
+    global end
     font = pygame.font.Font(None, 40)
     if cur_lvl == 1:
         text_surface = font.render(f"Собрано ключей: {keys}/1", True,
@@ -501,7 +543,8 @@ def inscriptions():
 
         if time_3 <= 0:
             time_3 = 22
-            return start_screen()
+            end = True
+            return level_completed()
 
     elif cur_lvl == 4:
         global time_4
@@ -523,7 +566,8 @@ def inscriptions():
 
         if time_4 <= 0:
             time_4 = 33
-            return start_screen()
+            end = True
+            return level_completed()
 
 
 def start_game(number):
